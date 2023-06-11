dbbd38934c67f2ea77bafeb0003e14198c688bed
julietowah <104780614+julietowah@users.noreply.github.com>
2023-06-11 11:20:54 +0100

    Update forms.py

--- project_portfolio/classroom/forms.py
+++ project_portfolio/classroom/forms.py
@@ -21,7 +21,7 @@ class LoginForm(forms.Form):
     )
 
 
-class SignUpForm(UserCreationForm):
+class StudentSignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -67,7 +67,58 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'is_student', 'is_teacher')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
+   
+
+class TeacherSignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
         
 
 
